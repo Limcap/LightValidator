df2fc13b706faee70ab090c12cb63475b24951c4
Leandro <l.lino@columbia.edu>
2023-06-03 02:25:53 -0300

    + EmailAddress struct

---
+++ EmailAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Limcap.LightValidator {
+	public struct EmailAddress {
+		public EmailAddress(string value) { Value = value; }
+		public string Value;
+		public bool IsValid { get => Regex.IsMatch(Value, @"^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.{2,3})+$"); }
+		public string UserName => !IsValid ? null : Value.Substring(0, Value.IndexOf('@'));
+		public string Provider => !IsValid ? null : Value.Substring('@');
+		public override string ToString() => Value;
+	}
+}
