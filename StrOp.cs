6ad7c387ab7ad9debeb09828fb2d64bf1e19f35f
Leandro <l.lino@columbia.edu>
2023-06-05 10:46:29 -0300

    StrOp moved to file and removed from proj

---
+++ StrOp.cs
@@ -0,0 +1,20 @@
+﻿namespace Limcap.LightValidator {
+	public enum StrOp {
+		Trim = 1, ToLower = 2, ToUpper = 4, RemoveDiacritics = 8, ToASCII = 16,
+		RemoveNonASCII = 32, RemoveNonWord = 64, RemoveNonDigits = 128,
+		Normalize = 11, NormalizeASCII = 27
+	}
+
+
+
+	public static class StrOpExtensions {
+		public static string Apply(this StrOp op, string x) {
+			if (op.HasFlag(StrOp.Trim)) { x = x.Trim(); }
+			if (op.HasFlag(StrOp.ToLower)) { x = x.ToUpper(); }
+			if (op.HasFlag(StrOp.ToUpper)) { x = x.ToUpper(); }
+			if (op.HasFlag(StrOp.RemoveDiacritics)) { x = x.RemoveDiacritics(); }
+			if (op.HasFlag(StrOp.ToASCII)) { x = x.ToASCII(); }
+			return x;
+		}
+	}
+}
